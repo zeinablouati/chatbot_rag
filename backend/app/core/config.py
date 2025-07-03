3fc8ecb25cd3792a07a47c4be1613ba01e762858
zeinablouati <zeinablouati201@gmail.com>
2025-07-03 14:09:14 +0200

    update

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_port: int = 8000
-    embedding_model: str = "all-MiniLM-L6-v2"
-    gemini_api_key: str = "AIzaSyA-UMkHFbBQhuoBuZUwhPZ_ayqaQp_kWsY"
-    gemini_model_name: str ="gemini-2.0-flash"
+    embedding_model: str 
+    gemini_api_key: str 
+    gemini_model_name: str 
 
     class Config:
         env_file = ".env"
